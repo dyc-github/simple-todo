c1cd1a091c3140ec4a18021a9f9ad7838099862d
David Choi <45525219+dyc-github@users.noreply.github.com>
2023-03-02 00:32:42 -0800

    checkpoint 2 finished

--- components/Task.js
+++ components/Task.js
@@ -1,9 +1,13 @@
-import {List, Checkbox, IconButton} from 'react-native-paper'
-const Task = (props)=>{
-    const taskText = props.taskText
-    return(
-        <List.Item title={taskText} left={()=>{<Checkbox.Android/>}} right={()=>{<IconButton/>}}/>
-    );
-}
+import { List, Checkbox, IconButton } from "react-native-paper";
+const Task = (props) => {
+  const taskText = props.taskText;
+  return (
+    <List.Item
+      title={"taskText"}
+      left={() => <Checkbox.Android />}
+      right={() => <IconButton icon='close'/>}
+    />
+  );
+};
 
 export default Task;
