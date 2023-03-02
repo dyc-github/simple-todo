abf2b85b7aec1a9d500b0c168571447b06d730d8
David Choi <45525219+dyc-github@users.noreply.github.com>
2023-03-02 00:05:22 -0800

    slight corrections

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import {Button} from 'react-native-paper';
 
 export default function App() {
@@ -17,7 +17,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
 });
