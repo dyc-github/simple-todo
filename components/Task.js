ca6acde2b03d540eb080a37ccf1d33551f2502d0
David Choi <45525219+dyc-github@users.noreply.github.com>
2023-03-02 22:45:59 -0800

    checkpoint3 complete

--- components/Task.js
+++ components/Task.js
@@ -1,13 +1,37 @@
+import { useState } from "react";
+import { StyleSheet, View } from "react-native";
 import { List, Checkbox, IconButton } from "react-native-paper";
 const Task = (props) => {
+  [isChecked, setIsChecked] = useState(false);
   const taskText = props.taskText;
   return (
     <List.Item
+      style={styles.task}
       title={"taskText"}
-      left={() => <Checkbox.Android />}
-      right={() => <IconButton icon='close'/>}
+      left={() => (
+        <View style={styles.checkboxContainer}>
+          <Checkbox.Android
+            status={isChecked ? 'checked' : 'unchecked'}
+            onPress={() => {
+              setIsChecked(!isChecked);
+            }}
+          />
+        </View>
+      )}
+      right={() => <IconButton icon="close" />}
     />
   );
 };
+
+const styles = StyleSheet.create({
+  checkboxContainer: {
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+  task: {
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+});
 
 export default Task;
