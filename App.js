53a64b7211483710a012c3c87db44426265a13b8
David Choi <45525219+dyc-github@users.noreply.github.com>
2023-03-03 01:43:17 -0800

    finished checkpoint 4

--- App.js
+++ App.js
@@ -1,12 +1,38 @@
 import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
 import { StyleSheet, View } from "react-native";
-import { Button } from "react-native-paper";
+import { Button, List } from "react-native-paper";
 import Task from "./components/Task";
+import uuid from 'react-native-uuid';
 
 export default function App() {
+  const [taskComponents, setTaskComponents] = useState([]);
+  const [taskData, setTaskData] = useState([]); //tasks will be formatted as such {id: taskID, taskText: taskText, isComplete: boolean}
+
+  const createTask = (taskText) => {
+    const newTaskDataEntry = { id: uuid.v4(), taskText: taskText, isComplete: false };
+    setTaskData([...taskData, newTaskDataEntry]);
+  };
+  const deleteTask = (id) => {
+    const newTaskData = taskData.filter(
+      (taskDataEntry) => taskDataEntry.id !== id
+    );
+    setTaskData(newTaskData);
+  };
+  const updateTaskIsComplete = (id, isComplete) => {
+    const newTaskData = taskData.map((taskDataEntry) => {
+      if (taskDataEntry.id === id) {
+        taskDataEntry.isComplete = isComplete;
+      }
+      return taskDataEntry;
+    });
+    setTaskData(newTaskData);
+  };
+
+
   return (
     <View style={styles.container}>
-      <Task taskText={'my first task'}/>
+      <List.Section>{taskComponents}</List.Section>
       <Button
         onPress={() => {
           console.log("hello world");
